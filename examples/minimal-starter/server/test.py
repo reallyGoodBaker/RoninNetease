885e1b56f9742a580b79be4146da2e16df9c5e9f
RGB39 <79316659+reallyGoodBaker@users.noreply.github.com>
2026-05-07 04:28:27 +0800

    chore: remove files & move folder

--- examples/minimal-starter/server/test.py
+++
@@ -1,7 +0,0 @@
-from ..architect.subsystem import ServerSubsystem, SubsystemServer
-
-
-@SubsystemServer
-class TestSubsystem(ServerSubsystem):
-    def onReady(self):
-        print "Test subsystem server is ready!"
